a5de0cc75cad0a78a90a2e635c5df04184f3403b
AnnyaB <anabordin2008@gmail.com>
2024-05-30 10:18:52 -0300

    Adicionar arquivos de projeto.

---
+++ aula02/Program.cs
@@ -0,0 +1,21 @@
+﻿using Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace aula02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Car car = new Car
+            {
+                Id = 1,
+                Name = "Carro Legal",
+                Color = "Azul",
+                Year = 1970
+            };
+            Console.WriteLine(car);
+        }
+    }
+}
